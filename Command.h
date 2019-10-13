d29a02f84850bc97a44eec9131a95928d44a2335
defaulterrr <sp@zero.ru>
2019-10-13 19:55:41 +0300

    Completed string engine

--- Command.h
+++ Command.h
@@ -3,10 +3,10 @@ using namespace std;
 
 struct Command{
     string wholeCommand;
-    string firstLevel;
-    string secondLevel;
-    string thirdLevel;
-    string fourthLevel;
+    // string firstLevel;
+    // string secondLevel;
+    // string thirdLevel;
+    // string fourthLevel;
 
     vector<string> commandList;
 
@@ -14,73 +14,44 @@ struct Command{
 
     Command(string command){
         this->wholeCommand = command;
+        wholeCommand += ' ';
         analyze();
-        //getFirstLevel();
     }
 
 private:
-    void getFirstLevel(){
-        /*bool lastWhitespaceBit = false;
-        bool lastNotWhiteSpace = false;
-        string tempfirstLevel = string();
-        for (unsigned int i=0;i<wholeCommand.length()-1;i++){
-            if (!lastWhitespaceBit){
-                if(wholeCommand[i+1] == ' '){}
-                    else {
-                        lastWhitespaceBit = true;
-                    }
-            }
-            else {
-                if()
-                tempfirstLevel+=wholeCommand[i];
-            }
-        }
-        */
-        int firstSymbolIndex;
-        int counter=0;
-        while(wholeCommand[counter]==' '){
-           counter++;
-        }
-        firstSymbolIndex = counter;
-        int lastSymbolIndex = wholeCommand.find_first_of(' ',counter);
-        this->firstLevel = wholeCommand.substr(firstSymbolIndex,lastSymbolIndex);
-    }
-
     void analyze(){
-        /*for (unsigned int i =0 ; i < wholeCommand.length() - 1; i++) {
-            if (wholeCommand[i] != ' ') {
-                string command;
-                int index = i;
-                int lastIndex = wholeCommand.find(i,' ');
-                command = wholeCommand.substr(i,lastIndex);
-                commandList.push_back(command);
-                cout << "Current iterator is " << i <<  endl;
-                i += lastIndex;
+        string minimalWhiteSpace = string();
+        for (unsigned int i=0; i< wholeCommand.size()-1;i++){
+            if(wholeCommand[i] != ' ' && wholeCommand[i]!='\n'){
+                minimalWhiteSpace += wholeCommand[i];
             }
-        }*/
-
-        for (unsigned int i = 0; i<wholeCommand.size()-1;i++) {
-            if(wholeCommand[i] == ' ' && wholeCommand[i+1] == '\n'){ break;} else { 
-                if (wholeCommand[i] == ' ') {
-                    cout << "Skipped whitespace" << endl;
-                    continue;
-
-                } else {
-                    int firstIndex = i;
-                    int lastIndex;
-                    for (unsigned int j = i;j<wholeCommand.size();j++) {
-                        if (wholeCommand[j] == ' ' or wholeCommand[j] == '\n') {
-                            lastIndex = j;
-                            break;
-                        }
-                    }
-                    string command = wholeCommand.substr(firstIndex, lastIndex);
-                    commandList.push_back(command);
-                    cout << "Added command " << command << endl;
-                    i = lastIndex-1;
-                }
+            else if (wholeCommand[i]==' ' && wholeCommand[i+1]!= ' '){
+                minimalWhiteSpace += wholeCommand[i];
             }
+            else if (wholeCommand[i+1]!=' ' && wholeCommand[i+2] == '\n') {
+                minimalWhiteSpace += wholeCommand[i+1];
+            }
+
         }
+
+        minimalWhiteSpace += ' ';
+        if(minimalWhiteSpace[0] == ' ') {} else
+        {minimalWhiteSpace = ' ' + minimalWhiteSpace;}
+
+        for (unsigned int i=0;i<minimalWhiteSpace.size();i++){
+            int first = minimalWhiteSpace.find_first_of(' ',i);
+            //cout << first;
+            int last = minimalWhiteSpace.find_first_of(' ',first+1);
+            //cout << " To " << last << endl;
+            i = first;
+            int length = last - first;
+            string message = minimalWhiteSpace.substr(first+1,length);
+            //cout << message << endl;
+            if (message == "") {} else
+            {commandList.push_back(message);}
+        }
+
+        //cout << "minimal white space is : " << minimalWhiteSpace << endl;
     }
     
 };
